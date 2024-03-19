5724e9ad93f25399e630ce5835e22288edac5de0
vasheekhan <162537702+vasheekhan@users.noreply.github.com>
2024-03-19 14:36:01 +0530

    Added my learning

---
+++ sigma wed dev couse/54 video/new.js
@@ -0,0 +1,4 @@
+console.log("hey i am console")
+console.log("hey i am ashish")
+console.log("hey i am vashe")
+console.log("hey i am owais")
---
+++ sigma wed dev couse/54 video/script.js
@@ -0,0 +1,10 @@
+console.log("hey i am console")
+var a=prompt("enter your number")
+var istrue=confirm("are you sure you want to leave this page and burn this computer ")
+if(istrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("computer is not blasting")
+}
+console.log("your number is "+a)
